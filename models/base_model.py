ca01d185fe7bd4c506592fe358f3b0fe3d63286d
leoworqu <leoworqu@gmail.com>
2023-12-10 01:43:23 -0800

    beqagn

--- models/base_model.py
+++ models/base_model.py
@@ -14,16 +14,16 @@ class BaseModel:
 
         """ Entry Point for Attributes """
 
-        format = "%Y-%m-%dT%H:%M:%S.%f"
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             self.updated_at = self.created_at
             storage.new(self)
         else
+        f = "%Y-%m-%dT%H:%M:%S.%f"
         for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], format)
+                    value = datetime.strptime(kwargs[key], f)
                 if key != '__class__':
                     setattr(self, key, value)
 
